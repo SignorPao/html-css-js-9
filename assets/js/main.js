a39da104cb8a634525b5f01cd5dd80caf9e66d76
Pavel Belousov <signorpao@gmail.com>
2024-03-28 17:38:03 +0300

    mobile menu done.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
 "use strict";
+
+// show / close mobile menu
+let menuBtn = document.querySelector(".menu"),
+  menuList = document.querySelector(".nav-bottom-menu"),
+  menuBtnIcon = menuBtn.querySelector("img");
+
+menuBtn.addEventListener("click", () => {
+  menuList.classList.toggle("open");
+  let isOpen = menuList.classList.contains("open");
+  menuBtnIcon.setAttribute(
+    "src",
+    isOpen ? "/assets/img/close.svg" : "/assets/img/menu.svg"
+  );
+});
